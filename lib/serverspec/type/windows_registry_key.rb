1ae94755e86763db8a7915b978e708eda4ac8fd3
sergueik <kouzmine_serguei@yahoo.com>
2015-09-28 19:52:23 -0400

    intermediate revision - added registry serverspec example

--- lib/serverspec/type/windows_registry_key.rb
+++ lib/serverspec/type/windows_registry_key.rb
@@ -15,5 +15,9 @@ module Serverspec::Type
     def has_property_value?(property_name, property_type, value)
       @runner.check_registry_key_has_value(@name, {:name => property_name, :type => property_type, :value => value})
     end
+
+    def has_propertyvaluecontaining?(property_name,  value)
+      @runner.check_registry_key_has_propertyvaluecontaining(@name, {:name => property_name, :value => value})
+    end
   end
 end
